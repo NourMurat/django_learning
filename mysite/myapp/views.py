fa42ff0a83b06645e3f899db65e664f86c32bd0f
Nour Murat <nurmurat111@gmai.com>
2024-10-26 18:19:31 +0400

    first commit

---
+++ mysite/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+def index(request):
+    items = Product.objects.all()
+    return HttpResponse(items)
+
+def contacts(request):
+    return render(request, "myapp/contacts.html")
+
+def about(request):
+    return HttpResponse("ABOUT")
